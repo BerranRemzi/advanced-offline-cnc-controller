f9f9950aa29efb9ad69e5be5b3b093448efa039e
Berran Remzi <11856339+BerranRemzi@users.noreply.github.com>
2021-08-15 15:02:09 +0300

    simple gcode reading

--- lib/AutoBedLeveling/src/main.cpp
+++ lib/AutoBedLeveling/src/main.cpp
@@ -4,6 +4,26 @@
 int main(void){
 	printf("int main(void)\n");
 	AutoBedLeveling();
+	
+	char filename[] = "bottom_contour.nc";
+    FILE *fp = fopen(filename, "r");
+
+    if (fp == NULL)
+    {
+        printf("Error: could not open file %s", filename);
+        return 1;
+    }
+
+    // reading line by line, max 256 bytes
+    const unsigned MAX_LENGTH = 256;
+    char buffer[MAX_LENGTH];
+
+    while (fgets(buffer, MAX_LENGTH, fp))
+        printf("%s", buffer);
+
+    // close the file
+    fclose(fp);
+	
 	printf("Press ENTER to exit...");
 	getchar();
 	return 0;
