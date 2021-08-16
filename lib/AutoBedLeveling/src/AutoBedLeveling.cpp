e198ed4f0d453bf97d876a92c27fb9de86ddcf57
Berk Cambaz <berkcambaz12321@gmail.com>
2021-08-16 18:32:36 +0300

    Add stdlib.h for atof

--- lib/AutoBedLeveling/src/AutoBedLeveling.cpp
+++ lib/AutoBedLeveling/src/AutoBedLeveling.cpp
@@ -1,19 +1,22 @@
 #include "./AutoBedLeveling.h"
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
-char* serialBuffer;
-int   soFar = 255;            // how much is in the buffer
+char *serialBuffer;
+int soFar = 255; // how much is in the buffer
 double posX;
-double posY;      // location
+double posY; // location
 
-void AutoBedLeveling(void) {
+void AutoBedLeveling(void)
+{
     printf("void AutoBedLeveling(void)\n");
 }
 
-void ABL_LoadLinePointer(char* line) {
+void ABL_LoadLinePointer(char *line)
+{
     serialBuffer = line;
 }
 
@@ -23,14 +26,17 @@ void ABL_LoadLinePointer(char* line) {
  * @input code the character to look for.
  * @input val the return value if /code/ is not found.
  **/
-bool ABL_ParseNumber(char code, double* pValue) {
-    char* ptr = serialBuffer;  // start at the beginning of buffer
-    while ((long) ptr > 1 && (*ptr) && (long) ptr < (long) serialBuffer + soFar) {  // walk to the end
-        if (*ptr == code) {  // if you find code on your walk,
-            *pValue = atof(ptr + 1);  // convert the digits that follow into a float and return it
+bool ABL_ParseNumber(char code, double *pValue)
+{
+    char *ptr = serialBuffer; // start at the beginning of buffer
+    while ((long)ptr > 1 && (*ptr) && (long)ptr < (long)serialBuffer + soFar)
+    { // walk to the end
+        if (*ptr == code)
+        {                            // if you find code on your walk,
+            *pValue = atof(ptr + 1); // convert the digits that follow into a float and return it
             return true;
         }
-        ptr = strchr(ptr, ' ') + 1;  // take a step from here to the letter after the next space
+        ptr = strchr(ptr, ' ') + 1; // take a step from here to the letter after the next space
     }
-    return false;  // end reached, nothing found, return default val.
+    return false; // end reached, nothing found, return default val.
 }
